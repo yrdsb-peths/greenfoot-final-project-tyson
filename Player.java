fa9b00a08a898cc61ff6c72124399e693b14f3bf
TysonPellatt <350019410@gapps.yrdsb.ca>
2022-05-31 10:55:16 -0400

    player stuff

--- Player.java
+++ Player.java
@@ -10,23 +10,68 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 public class Player extends Actor
 {
     String facing = "right";
-    GreenfootImage leftCycle = new GreenfootImage("images/SpriteLeft/left00.png");
-    //GreenfootImage[] rightCycle = new GreenfootImage[4];
-    //GreenfootImage[] upCycle = new GreenfootImage[4];
-    //GreenfootImage[] downCycle = new GreenfootImage[4];
+    SimpleTimer animationTime = new SimpleTimer();
+    int imageIndex = 0;
+    GreenfootImage[] leftCycle = new GreenfootImage[4];
+    GreenfootImage[] rightCycle = new GreenfootImage[4];
+    GreenfootImage[] upCycle = new GreenfootImage[4];
+    GreenfootImage[] downCycle = new GreenfootImage[4];
     /**
      * Act - do whatever the Player wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
     public Player()
     {
-        /**
         for(int i =0; i < leftCycle.length; i++)
         {
-            leftCycle = new GreenfootImage("images/SpriteLeft/left0" + i +".png");
-            leftCycle.scale(35,35);
-        }*/
-        setImage(leftCycle);
+            leftCycle[i] = new GreenfootImage("images/SpriteLeft/left0"+ i +".png");
+            leftCycle[i].scale(35,35);
+        }
+        for(int i =0; i < rightCycle.length; i++)
+        {
+            rightCycle[i] = new GreenfootImage("images/SpriteRight/right0"+ i +".png");
+            rightCycle[i].scale(35,35);
+        }
+        for(int i =0; i < upCycle.length; i++)
+        {
+            upCycle[i] = new GreenfootImage("images/SpriteUp/up0"+ i +".png");
+            upCycle[i].scale(35,35);
+        }
+        for(int i =0; i < downCycle.length; i++)
+        {
+            downCycle[i] = new GreenfootImage("images/SpriteDown/down0"+ i +".png");
+            downCycle[i].scale(35,35);
+        }
+        animationTime.mark();
+        setImage(downCycle[1]);
+    }
+    public void animate()
+    {
+        if(animationTime.millisElapsed() < 400)
+        {
+            return;
+        }
+        animationTime.mark();
+        if(facing.equals("right"))
+        {
+            setImage(rightCycle[imageIndex]);
+            imageIndex = (imageIndex + 1) % rightCycle.length; 
+        }
+        if(facing.equals("left"))
+        {
+            setImage(leftCycle[imageIndex]);
+            imageIndex = (imageIndex + 1) % leftCycle.length; 
+        }
+        if(facing.equals("up"))
+        {
+            setImage(upCycle[imageIndex]);
+            imageIndex = (imageIndex + 1) % upCycle.length; 
+        }
+        if(facing.equals("down"))
+        {
+            setImage(downCycle[imageIndex]);
+            imageIndex = (imageIndex + 1) % downCycle.length; 
+        }
     }
     public void act()
     {
@@ -51,6 +96,8 @@ public class Player extends Actor
             facing = "down";
             setLocation(getX(),getY()+2);
         }
+        wallColision();
+        animate();
     }
     public void wallColision()
     {
