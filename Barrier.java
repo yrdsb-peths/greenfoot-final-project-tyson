40f97cbaaa88c2bc66262c9632a2014575ddc55c
TysonPellatt <350019410@gapps.yrdsb.ca>
2022-05-31 10:39:02 -0400

    game start up

--- Barrier.java
+++ Barrier.java
@@ -8,12 +8,20 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class Barrier extends Actor
 {
+    GreenfootImage wall = new GreenfootImage("images/wallsArt/moss2.png");
     /**
      * Act - do whatever the Barrier wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
+    public Barrier()
+    {
+        setImage(wall);
+        wall.scale(40,40);
+    }
     public void act()
     {
         // Add your action code here.
+        
+        
     }
 }
--- Player.java
+++ Player.java
@@ -10,48 +10,63 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 public class Player extends Actor
 {
     String facing = "right";
+    GreenfootImage leftCycle = new GreenfootImage("images/SpriteLeft/left00.png");
+    //GreenfootImage[] rightCycle = new GreenfootImage[4];
+    //GreenfootImage[] upCycle = new GreenfootImage[4];
+    //GreenfootImage[] downCycle = new GreenfootImage[4];
     /**
      * Act - do whatever the Player wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
+    public Player()
+    {
+        /**
+        for(int i =0; i < leftCycle.length; i++)
+        {
+            leftCycle = new GreenfootImage("images/SpriteLeft/left0" + i +".png");
+            leftCycle.scale(35,35);
+        }*/
+        setImage(leftCycle);
+    }
     public void act()
     {
         // Add your action code here.
         if(Greenfoot.isKeyDown("left"))
         {
             facing = "left";
-            move(-5);
-        }
-        if(Greenfoot.isKeyDown("a"))
-        {
-            facing = "left";
-            move(-5);
+            move(-2);
         }
         if(Greenfoot.isKeyDown("right"))
         {
             facing = "right";
-            move(5);
-        }
-        if(Greenfoot.isKeyDown("d"))
-        {
-            facing = "right";
-            move(5);
+            move(2);
         }
         if(Greenfoot.isKeyDown("up"))
         {
-            setLocation(getX(),getY()-2);
-        }
-        if(Greenfoot.isKeyDown("w"))
-        {
+            facing = "up";
             setLocation(getX(),getY()-2);
         }
         if(Greenfoot.isKeyDown("down"))
         {
+            facing = "down";
             setLocation(getX(),getY()+2);
         }
-        if(Greenfoot.isKeyDown("s"))
-        {
-            setLocation(getX(),getY()+2);
+    }
+    public void wallColision()
+    {
+        if(isTouching(Barrier.class)){
+            if(facing.equals("right")){
+                setLocation(getX()-2,getY());
+            }
+            if(facing.equals("left")){
+                setLocation(getX()+2,getY());
+            }
+            if(facing.equals("up")){
+                setLocation(getX(),getY()-2);
+            }
+            if(facing.equals("right")){
+                setLocation(getX(),getY()+2);
+            }
         }
     }
 }
